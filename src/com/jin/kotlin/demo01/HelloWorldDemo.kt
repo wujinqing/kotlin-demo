86b7d6f9ff63926d776f906673269e2965f596c0
wujq4git <1140601053@qq.com>
2017-10-30 22:22:19 +0800

    Initial commit

---
+++ src/com/jin/kotlin/demo01/HelloWorldDemo.kt
@@ -0,0 +1,24 @@
+package com.jin.kotlin.demo01
+
+import java.util.function.Consumer
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    println("---------------")
+
+    val list: List<String> = listOf("hello", "world", "hello world")
+
+    for (str in list)
+    {
+        println(str)
+    }
+
+    println("---------------")
+
+    list.forEach(Consumer { println(it) })
+
+    println("---------------")
+
+    list.forEach(System.out::println)
+}
