da1d1c6311369a1d94dc136b679bf732a3e29844
wujinqing <wujinqing@jetair.com.cn>
2017-11-08 18:30:04 +0800

    commit

---
+++ src/com/jin/kotlin/HelloKotlin6.kt
@@ -0,0 +1,48 @@
+package com.jin.kotlin
+
+/**
+ * @author wu.jinqing
+ * @date 2017年11月08日
+ *
+ */
+fun main(args: Array<String>) {
+    // IntArray -> int[]
+//    var array:IntArray = intArrayOf(1, 2, 3, 4, 5)
+    var array = intArrayOf(1, 2, 3, 4, 5)
+
+    // 按元素遍历
+    for(item: Int in array)
+    {
+        println(item)
+    }
+
+    println("--------------")
+
+    for (item in array)
+    {
+        println(item)
+    }
+
+    println("--------------")
+
+    // 按索引遍历
+    for(i: Int in array.indices)
+    {
+        println("array[$i] = ${array[i]}")
+    }
+
+    println("--------------")
+
+    for(i in array.indices)
+    {
+        println("array[$i] = ${array[i]}")
+    }
+
+    println("--------------")
+
+    // 按元素和索引遍历
+    for((index, value) in array.withIndex())
+    {
+        println("array[${index}] = ${value}")
+    }
+}
---
+++ src/com/jin/kotlin/HelloKotlin7.kt
@@ -0,0 +1,74 @@
+package com.jin.kotlin
+
+/**
+ * when 关键字
+ *
+ * @author wu.jinqing
+ * @date 2017年11月08日
+ *
+ */
+fun main(args: Array<String>) {
+    println(myPrint("hello"))
+    println(myPrint("world"))
+    println(myPrint("test"))
+
+    println("---------------")
+
+    println(myPrint2("hello"))
+    println(myPrint2("world"))
+    println(myPrint2("test"))
+
+    println("---------------")
+
+    var a = 30
+
+    var result = when(a) {
+        1 -> {
+            println("a = 1")
+            10
+        }
+        2 -> {
+            println("a = 2")
+            20
+        }
+        3, 4, 5 -> {
+            println("a = 3 or 4 or 5")
+            30
+        }
+        // kotlin中的区间左闭区间右也是闭区间，java中的区间左闭区间右是开区间
+        in 6..10 -> {
+            println("a is between 6 and 10")
+            40
+        }
+
+        else -> {
+            println("a is other value")
+            50
+        }
+    }
+
+    println("result = ${result}")
+}
+
+fun myPrint(str: String): String
+{
+//    when (str)
+//    {
+//        "hello" -> return "HELLO"
+//        "world" -> return "EORLD"
+//        else -> return "other input"
+//    }
+
+    return when (str) {
+        "hello" -> "HELLO"
+        "world" -> "EORLD"
+        else -> "other input"
+    }
+}
+
+fun myPrint2(str: String) =
+        when (str) {
+            "hello" -> "HELLO"
+            "world" -> "EORLD"
+            else -> "other input"
+        }
