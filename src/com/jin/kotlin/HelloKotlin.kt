86d8a24566cc1de8042c77e382a65f6c5f9645cf
wujinqing <wujinqing@jetair.com.cn>
2018-01-22 17:45:59 +0800

    commit

--- src/com/jin/kotlin/HelloKotlin.kt
+++ src/com/jin/kotlin/HelloKotlin.kt
@@ -9,9 +9,49 @@ package com.jin.kotlin
 fun main(args: Array<String>)
 {
     println(sum(1, 2))
+    println(sum2(1, 2))
+    myPrint(1, 2)
+    myPrint1(1, 2)
 }
 
+// 返回类型在方法名后面
 fun sum(a: Int, b: Int): Int
 {
     return a + b
 }
+
+// 函数体只有一行
+fun sum2(a: Int, b:Int) = a +b
+
+// 没有返回值用Unit(也可以省略掉)，类似于java的void
+fun myPrint(a: Int, b:Int): Unit
+{
+    println(a + b)
+}
+
+// 字符串模板，可以在字符串里面引用变量的值，通过$符号来引用, 如果是表达式的话用花括号表示
+fun myPrint1(a: Int, b: Int)
+{
+    println("$a + $b = ${a + b}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
